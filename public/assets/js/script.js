ec6280d1df7cbe574b3751a15a5b8280deef0d51
MajidRehmann <majidrehman036@gmail.com>
2024-01-24 11:50:48 +0500

    Form done+ image updated+ recaptcha done

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -123,3 +123,79 @@ scriptElement.domain = "www.chatbase.co";
 scriptElement.defer = true;
 
 document.head.appendChild(scriptElement);
+
+
+
+
+
+
+/**
+Form Submission (Firebase and send email)
+**/
+
+
+/** Firebase account data */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLmQ31_wkYljUhIjaixjCEtVSNW3pxNzE",
+  authDomain: "hammad-tariq-com.firebaseapp.com",
+  databaseURL: "https://hammad-tariq-com.firebaseio.com",
+  projectId: "hammad-tariq-com",
+  storageBucket: "hammad-tariq-com.appspot.com",
+  messagingSenderId: "221244520282",
+  appId: "1:221244520282:web:63da9cf58697ce6f44a563"
+};
+
+firebase.initializeApp(firebaseConfig);
+var messagesRef = firebase.database().ref('messages');
+
+document.getElementById('contactform').addEventListener('submit', submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message to Firebase
+  saveMessage(name, email, phone, message);
+
+  // Send email using email.js
+  emailjs.send("service_i7hz09b", "template_rfhi4o3", {
+    to_email: email,
+    from_name: name,
+    from_email: email, 
+    message: message 
+  })
+  .then(function(response) {
+    console.log('Email sent successfully:', response);
+
+    // Display success message
+    document.querySelector('.alert').style.display = 'block';
+
+    // Reset the form after a delay
+    setTimeout(function () {
+      document.querySelector('.alert').style.display = 'none';
+      document.getElementById('contactform').reset();
+    }, 1000);
+  })
+  .catch(function(error) {
+    console.error('Error sending email:', error);
+    // Handle the error
+  });
+}
+
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
+
+function saveMessage(name, email, phone, message) {
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email: email,
+    phone: phone,
+    message: message
+  });
+}
